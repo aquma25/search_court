66fe0fbfc913785847ec5faddd7e62c290e425e2
aquma25 <h.aquma25@gmail.com>
2020-07-05 17:06:05 +0900

    index_design

--- app/assets/javascripts/play_grounds.js
+++ app/assets/javascripts/play_grounds.js
@@ -4,10 +4,6 @@ function initMap(){
   var play_grounds_json = document.getElementById("play_grounds_json").value;
   var grounds_array = JSON.parse(play_grounds_json);
 
-  // 初期のプレイグラウンド名を複数表示
-  var courtName = document.getElementById("court_name")
-  courtName.innerHTML = grounds_array[0].court_name;
-
   // 初期の緯度、軽度の設定
   init_latlng = {
     lat: grounds_array[0].latitude,
@@ -17,10 +13,10 @@ function initMap(){
   var mapLatLng = new google.maps.LatLng(init_latlng);
   map = new google.maps.Map(document.getElementById('map'), {
     center: mapLatLng, // 地図の中心を指定
-    zoom: 12 // 地図のズームを指定
+    zoom: 10 // 地図のズームを指定
   });
 
-  // コート情報を取得していく
+  // 複数のコート情報を取得していく
   for (const ground of grounds_array) {
     latlng = {
       lat: ground.latitude,
@@ -33,21 +29,20 @@ function initMap(){
       map: map // マーカーを立てる地図を指定
     });
 
-    var showUrl = "</br><a href=" + "http://" + rails_path + "/play_grounds/" + ground.id + ">" + "詳細を表示する" + "</a>"
-    commentInMarker(ground.court_name + showUrl, myMarker)
+    var showUrl = "</br><a href=" + "http://" + rails_path + "/play_grounds/" + ground.id + ">" + "詳細を表示" + "</a>"
+    dispCommentInMarker(ground.court_name + showUrl, myMarker)
   }
 }
 
-// マーカーに吹き出しを表示出来るようにする
-function commentInMarker(comment, myMarker) {
+// マーカーをクリックして吹き出しを表示出来るようにする
+function dispCommentInMarker(comment, myMarker) {
   var myInfoWindow = new google.maps.InfoWindow({
-    content: comment // 吹き出しに出す文
+    content: comment
   });
-  myInfoWindow.open(map, myMarker);
-  // 吹き出しが閉じられたら、マーカークリックで再び開くようにしておく
-  google.maps.event.addListener(myInfoWindow, "closeclick", function() {
-    google.maps.event.addListenerOnce(myMarker, "click", function(event) {
-      myInfoWindow.open(map, myMarker);
+  google.maps.event.addListener(myMarker, "click", function() {
+    myInfoWindow.open(map, myMarker);
+    google.maps.event.addListenerOnce(myInfoWindow, "closeclick", function(event) {
+      myInfoWindow.close(map, myMarker);
     });
   });
 }
@@ -55,7 +50,7 @@ function commentInMarker(comment, myMarker) {
 // ShowMapボタン押下でMap上に該当プレイグラウンドのマーカーを追加していく
 function dispMarker(lat, lng, id, indx_num, inout, path) {
   var courtPlace = document.getElementById(inout + "court_" + indx_num).value;
-  var showUrl = "</br><a href=" + "http://" + path + "/play_grounds/" + id + ">" + "詳細を表示する" + "</a>"
+  var showUrl = "</br><a href=" + "http://" + path + "/play_grounds/" + id + ">" + "詳細を表示" + "</a>"
   court_name.innerHTML = courtPlace;
 
   latlng = {
@@ -73,5 +68,20 @@ function dispMarker(lat, lng, id, indx_num, inout, path) {
   });
   map.setCenter(new google.maps.LatLng(latlng));
 
-  commentInMarker(court_name.innerHTML + showUrl, myMarker)
+  openCommentInMarker(court_name.innerHTML + showUrl, myMarker)
+  scrollTo(0, 0);
+}
+
+// マーカーに吹き出しを表示出来るようにする
+function openCommentInMarker(comment, myMarker) {
+  var myInfoWindow = new google.maps.InfoWindow({
+    content: comment // 吹き出しに出す文
+  });
+  myInfoWindow.open(map, myMarker);
+  // 吹き出しが閉じられたら、マーカークリックで再び開くようにしておく
+  google.maps.event.addListener(myInfoWindow, "closeclick", function() {
+    google.maps.event.addListenerOnce(myMarker, "click", function(event) {
+      myInfoWindow.open(map, myMarker);
+    });
+  });
 }
