e6eee460bd91711c9f5f5b8e463ef8eb876affdc
aquma25 <h.aquma25@gmail.com>
2020-08-10 18:21:56 +0900

    showページのリファクタ

--- app/models/play_ground.rb
+++ app/models/play_ground.rb
@@ -20,4 +20,38 @@ class PlayGround < ApplicationRecord
     place_infos = Geocoder.search(search_address)
     place_infos.empty? ? default_criteria_latlng(marker_info) : { lat: place_infos[0].data["lat"], lng: place_infos[0].data["lon"] }
   end
+
+  # 時間と分のみを表示する(ex: "19:00")
+  def self.hm_time_format(time)
+    time.strftime("%H:%M")
+  end
+
+  # 利用可能時間帯を表示する
+  def self.utilization_time(start_time, end_time)
+    return "時間指定なし" if start_time.nil? || end_time.nil?
+    hm_time_format(start_time) + " ~ " + hm_time_format(end_time)
+  end
+
+  # 利用料金が必要かどうか
+  def self.usage_fee_of_existence(usage_fee)
+    usage_fee.nil? ? "なし" : usage_fee + "円"
+  end
+
+  def self.usage_week_of_existence(usage_week)
+    usage_week.nil? ? "毎日" : usage_week
+  end
+
+  def self.disp_court_infos(court_infos)
+    {
+      name:             court_infos&.court_name,
+      time_zone:        utilization_time(court_infos&.start_time, court_infos&.end_time),
+      nearest_station:  court_infos&.nearest_station + "駅",
+      address:          court_infos&.address,
+      place:            court_infos&.place,
+      usage_fee:        usage_fee_of_existence(court_infos&.usage_fee),
+      usage_week:       usage_week_of_existence(court_infos&.usage_week),
+      status:           court_infos&.status,
+      content:          court_infos&.content
+    }
+  end
 end
