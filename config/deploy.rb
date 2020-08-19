2e2219ea2a58d3c9111eb331e68e1f2d4cb60fbf
aquma25 <h.aquma25@gmail.com>
2020-08-19 18:55:51 +0900

    bugfix

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.13.0"
 
 set :application, "search_court"
-set :repo_url, "git@example.com:aquma25/search_court.git"
+set :repo_url, "git@github.com:aquma25/search_court.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
